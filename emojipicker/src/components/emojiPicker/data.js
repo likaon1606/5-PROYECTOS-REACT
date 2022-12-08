de7c407016bd65fcc3e44751c9ab9a6bbc742916
Ariel <ariel.fuentesgarcia@gmail.com>
2022-12-08 16:53:32 -0600

    emojis finish

---
+++ emojipicker/src/components/emojiPicker/data.js
@@ -0,0 +1,17 @@
+export const data = [
+  {
+    symbol: "😊",
+    name: "cara alegre",
+    keywords: "smiling face happy",
+  },
+  {
+    symbol: "💗",
+    name: "corazon",
+    keywords: "heart love like",
+  },
+  {
+    symbol: "🔥",
+    name: "fuego",
+    keywords: "fire hot burn",
+  },
+];
